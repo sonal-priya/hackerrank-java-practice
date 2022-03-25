c209df60329ae459af9d87046e614299e3804578
Sonal Priya <sonalpriyassm@gmail.com>
2022-03-25 23:02:31 +0530

    Static block initializer

--- src/StaticInitializerBlock.java
+++ src/StaticInitializerBlock.java
@@ -1,18 +1,33 @@
+
+import java.util.Scanner;
+
 public class StaticInitializerBlock {
-    // Write your code here
-    static {
-        Scanner sc = new Scanner(System.in);
-        int B = sc.nextInt();
-        int H = sc.nextInt();
-        boolean flag = B > 0 && H > 0;
-    }
-
-    public static void main(String[] args) {
-        if (flag) {
-            int area = B * H;
-            System.out.print(area);
+    //Write your code here
+    static Scanner sc = new Scanner(System.in);
+    static int B = sc.nextInt();
+    static int H = sc.nextInt();
+    static boolean flag = true;
+    
+    static{
+        try{
+            if(B<=0 || H<=0){
+                flag = false;
+                throw new Exception("java.lang.Exception: Breadth and height must be positive");
+            }
+        }catch(Exception e){
+            System.out.print(e.getMessage());
         }
+    }
+    
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
 
-    }// end of main
+}//end of class
 
-}
+
+
